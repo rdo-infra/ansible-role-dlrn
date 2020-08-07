d3fa1e695899aa6fa50d54c3fef5a33fc8f1a090
Javier Pena <jpena@redhat.com>
2020-08-07 15:17:24 +0200

    Initial commit, DLRN common configuration
    This commit adds the common DLRN configuration parts, ported from
    puppet-dlrn.
    
    Change-Id: Ief959e513eb9cce5f7c1fffbb9b83f7b6ab4972b

---
+++ molecule/ssl/tests/test_ssl_web.py
@@ -0,0 +1,56 @@
+# Copyright 2020 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import testinfra
+
+testinfra_hosts = ['all']
+
+
+def test_httpd_is_running(host):
+     assert host.service("httpd").is_running
+
+def test_vhost_files(host):
+    vhost_file = host.file('/etc/httpd/conf.d/10-trunk.rdoproject.org.conf')
+    ssl_file = host.file('/etc/httpd/conf.d/10-ssl-trunk.rdoproject.org.conf')
+    
+    assert vhost_file.exists
+    assert ssl_file.exists
+    assert b'Redirect permanent / https://trunk.rdoproject.org/' in vhost_file.content
+    assert b'<Location "/api-rhel8-master">' in vhost_file.content
+    assert b'<Location "/api-redhat-master">' in vhost_file.content
+    assert b'<Location "/api-centos8-train">' in vhost_file.content
+    assert b'WSGIDaemonProcess dlrn-centos-stein python-home=' in vhost_file.content
+    assert b'<Location "/api-rhel8-master">' in ssl_file.content
+    assert b'<Location "/api-redhat-master">' in ssl_file.content
+    assert b'<Location "/api-centos8-train">' in ssl_file.content
+    assert b'WSGIDaemonProcess dlrn-centos-stein python-home=' in vhost_file.content
+
+def test_rdo_files(host):
+    logo_file = host.file('/var/www/html/images/rdo-logo-white.png')
+    index_file = host.file('/var/www/html/index.html')   
+    assert logo_file.exists
+    assert index_file.exists
+
+def test_http_ports(host):
+    assert host.socket("tcp://0.0.0.0:80").is_listening
+    assert host.socket("tcp://0.0.0.0:443").is_listening
+
+def test_packages(host):
+    assert host.package("httpd").is_installed
+    assert host.package("mod_ssl").is_installed
+    assert host.package("python3-mod_wsgi").is_installed
+
+def test_cron(host):
+    cmd = host.run('crontab -l -u root')
+    assert '/usr/local/bin/update-web-index.sh' in cmd.stdout
