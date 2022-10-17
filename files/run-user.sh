0e7e4890d7b91b74986e621dfb3de1d031b7c5c2
Enrique Vallespi Gil <evallesp@redhat.com>
2022-10-17 17:08:54 +0200

    Add run-user.sh script.
    With this script we are able to execute dlrn-user command in a safety way.
    Also we are able to log the output in a file as done in run-dlrn or run-purge scripts.
    
    Change-Id: Ide72ae6530a2e4759bebcf5c15b2456c043464c1

---
+++ files/run-user.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+LOCK="/home/${USER}/dlrn.lock"
+set -e
+
+# if any arguments are given, assume console foreground execution
+ARG=$1
+
+exec 200>$LOCK
+if !  flock -n 200
+then
+    if [ -n "$ARG" ]
+    then
+        echo "DLRN ${USER} running, please try again after disabling it."
+    fi
+    exit 1
+fi
+
+if [ ! -d /home/${USER}/dlrn-logs ]
+then
+    mkdir -p /home/${USER}/dlrn-logs
+fi
+
+source ~/.venv/bin/activate
+LOGFILE=/home/${USER}/dlrn-logs/dlrn-user.$(date +%s).log
+cd ~/dlrn
+
+set +e
+echo `date` "Starting dlrn-user." >> $LOGFILE
+dlrn-user --config-file /usr/local/share/dlrn/${USER}/projects.ini "$@" 2>> $LOGFILE
+RET=$?
+echo `date` "dlrn-user complete." >> $LOGFILE
+
+if [ -n "$ARG" ]
+then
+    echo Arguments: "$@"
+    cat $LOGFILE
+fi
+exit $RET
